3d4b587179a3ec95f8d6da37239d1572a96ad5e2
Amir <amir110ghoolizadeh@gmail.com>
2019-08-09 14:36:06 +0430

    finish js

--- js/app.js
+++ js/app.js
@@ -6,9 +6,37 @@ function random(from, to){
 }
 class Action{
     constructor(){
-        this.withoutS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'];;
-        this.withS = this.withoutS;
-        this.withS.push('$','@','&','_','-');
+        this.withoutS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'];
+        this.s = ['$','@','_','-','&'];
+        this.withS = this.withoutS.concat(this.s);
+        this.rand = 0;
+        this.generateString = '';
+    }
+    static setOutput(pass){
+        $('.main__output').style.display="block";
+        $('#generate').textContent = pass;
+    }
+    static generate(list){
+        for(let i = 0;i < 20;i++){
+            this.rand = random(0, list.length);
+             this.generateString+=  list[this.rand];
+            }
+            Action.setOutput(this.generateString);
+            this.generateString = '';
+    }
+     withs(){
+        Action.generate(this.withS);
+    }
+     withouts(){
+        Action.generate(this.withoutS);
     }
 } 
-
+const act = new Action();
+$('#with').addEventListener('click',e => {
+    act.withs();
+    e.preventDefault();
+});
+$('#without').addEventListener('click',e => {
+    act.withouts();
+    e.preventDefault();
+});
